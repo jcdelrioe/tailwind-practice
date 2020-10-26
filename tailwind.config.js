5658077ad459c32f53db52b50b6f7490ad418e00
Juan Carlos del Rio <jcdre@hotmail.com>
2020-10-26 16:16:14 -0500

    Upload proyect to github

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'primary':'#f3c614',
+        'secondary':'#353535'
+      },
+      height: {
+        '80': '20rem',
+        '100': '25rem'
+      },
+    },
+  },
+  variants: {
+  },
+  plugins: [],
+}
